8d15522181fd3b7128a4e4978ccd3097d08ab157
zhangxiaohui <zhangxh2000@126.com>
2026-05-13 21:45:20 +0800

    修改compileSdk版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.zhangxh.subtitletranslator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.zhangxh.subtitletranslator"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
